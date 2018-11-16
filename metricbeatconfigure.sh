cdea7baa8d48c35f6ebebb22f58d966ac6e8aae2
Michelkr <45073572+Michelkr@users.noreply.github.com>
2018-11-16 07:16:42 +0100

    Add files via upload

---
+++ metricbeatconfigure.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
